d223cdfd608571f24670a40bc3640c7caa984cfb
SeanMcOwen <seanmcowen@gmail.com>
2025-02-14 14:28:48 -0500

    System processor loading

--- src/bdp_lib/system.py
+++ src/bdp_lib/system.py
@@ -8,6 +8,30 @@ class System:
         else:
             self.description = None
 
+        self._load_processors(json["Processors"], processors_map)
+        self._load_wires(json["Wires"], wires_map)
+        self._check_ports()
+
+    def _load_processors(self, processors, processors_map):
+        bad_processors = [
+            processor for processor in processors if processor not in processors_map
+        ]
+        assert (
+            len(bad_processors) == 0
+        ), "The system {} references processor IDs of {} which are not valid processor IDs".format(
+            self.name, bad_processors
+        )
+        self.processors = [processors_map[processor] for processor in processors]
+
+        # CHECK DUPLICATES
+
+    def _load_wires(self, wires, wires_map):
+        pass
+
+    def _check_ports(self):
+        # Check only one wire into each port and warn if any ports are not filled
+        pass
+
 
 def load_system(json, processors_map, wires_map):
     return System(json, processors_map, wires_map)
