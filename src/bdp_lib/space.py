8a9b11d989b213998efa62f807cf95f910e3f7cc
SeanMcOwen <seanmcowen@gmail.com>
2025-02-14 12:14:30 -0500

    Spaces loading

--- src/bdp_lib/space.py
+++ src/bdp_lib/space.py
@@ -1,6 +1,12 @@
 class Space:
     def __init__(self, json: dict):
         self.raw_data = json
+        self.id = json["ID"]
+        self.name = json["Name"]
+        if "Description" in json:
+            self.description = json["Description"]
+        else:
+            self.description = None
 
 
 def load_space(json: dict):
