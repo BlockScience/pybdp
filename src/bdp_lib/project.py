47b624fc436503d74290abcd4bea845aadc37a05
SeanMcOwen <seanmcowen@gmail.com>
2025-02-14 12:37:20 -0500

    Duplicates at project level

--- src/bdp_lib/project.py
+++ src/bdp_lib/project.py
@@ -2,6 +2,7 @@ from jsonschema import validate
 from .schema import schema
 from .toolbox import load_toolbox
 from .workbench import load_workbench
+from .convenience import find_duplicates
 
 
 class Project:
@@ -12,12 +13,24 @@ class Project:
         self.blocks = self.toolbox.blocks
         self.spaces = self.toolbox.spaces
 
+        self.processors = []
+        self.wires = []
+        self.systems = []
+
         # Bring in mapping
         self.blocks_map = self.toolbox.blocks_map
         self.spaces_map = self.toolbox.spaces_map
         self.toolbox_map = self.toolbox.toolbox_map
 
-        print("Implement validation that there are no duplicate IDs")
+        self._validate_unique_ids()
+
+    def _validate_unique_ids(self):
+        duplicates = find_duplicates(
+            self.blocks + self.spaces + self.processors + self.wires + self.systems
+        )
+        assert (
+            len(duplicates) == 0
+        ), f"Overlapping IDs between the toolbox and workbench found: {duplicates}"
 
 
 def load_project(json: dict):
