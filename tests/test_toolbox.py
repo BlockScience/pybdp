f95b9d7b6a17fcc8c8033c58b9c0f9d6998e889f
SeanMcOwen <seanmcowen@gmail.com>
2025-03-10 23:24:00 -0400

    Fixture

--- tests/test_toolbox.py
+++ tests/test_toolbox.py
@@ -1,13 +1,18 @@
+import pytest
 from common import spaces, blocks
 from pybdp.toolbox import Toolbox
 
 
-def test_toolbox_spaces():
+@pytest.fixture(scope="module")
+def toolbox():
     json = {
         "Spaces": spaces,
         "Blocks": blocks,
     }
-    toolbox = Toolbox(json)
+    return Toolbox(json)
+
+
+def test_toolbox_spaces(toolbox):
     assert len(toolbox.spaces) == 5
     assert toolbox.spaces[0].id == "S1"
     assert toolbox.spaces[1].id == "S2"
@@ -21,12 +26,7 @@ def test_toolbox_spaces():
     assert str(toolbox.spaces[4]) == "< Space ID: S5 Name: E >"
 
 
-def test_toolbox_blocks():
-    json = {
-        "Spaces": spaces,
-        "Blocks": blocks,
-    }
-    toolbox = Toolbox(json)
+def test_toolbox_blocks(toolbox):
     assert len(toolbox.blocks) == 3
     assert toolbox.blocks[0].id == "B1"
     assert toolbox.blocks[1].id == "B2"
@@ -36,12 +36,7 @@ def test_toolbox_blocks():
     assert str(toolbox.blocks[2]) == "< Block ID: B3 Name: Block 3 ['E', 'B']->['D']>"
 
 
-def test_toolbox_mappings():
-    json = {
-        "Spaces": spaces,
-        "Blocks": blocks,
-    }
-    toolbox = Toolbox(json)
+def test_toolbox_mappings(toolbox):
     assert len(toolbox.blocks_map) == 3
     assert toolbox.blocks_map["B1"].id == "B1"
     assert toolbox.blocks_map["B2"].id == "B2"
